1429682ca7f838206939f690d064df1a8a2b977f
userquin <userquin@gmail.com>
2024-11-15 22:27:01 +0100

    chore: remove empty `types.cjs` and `types.mjs` from dist

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,9 @@
+import { rm } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
+
+const root = fileURLToPath(new URL('./dist', import.meta.url))
 
 export default defineBuildConfig({
   entries: [
@@ -8,10 +13,18 @@ export default defineBuildConfig({
     'src/constants.ts',
     'src/types.ts',
   ],
-  clean: true,
+  clean: false,
   declaration: true,
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
+  },
+  hooks: {
+    'build:done': async () => {
+      await Promise.all([
+        rm(resolve(root, 'types.cjs')),
+        rm(resolve(root, 'types.mjs')),
+      ])
+    },
   },
 })
